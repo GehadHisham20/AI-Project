39dd551efc5ffec8f412ca7d34b8108cf27e9e89
tokamh <73889436+tokamh@users.noreply.github.com>
2022-05-25 11:48:39 -0700

    Update csp_in_progress.py

--- csp_in_progress.py
+++ csp_in_progress.py
@@ -1,4 +1,14 @@
+def first(iterable, default=None):
+    try:
+        return iterable[0]
+    except IndexError:
+        return default
+    except TypeError:
+        return next(iterable, default)
 
+def is_in(elt, seq):
+    return any(x is elt for x in seq)
+    
 def count(seq):
     return sum(bool(x) for x in seq)
 
