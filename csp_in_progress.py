07cbee942f320bda8490e1f49c9817c14d2095b6
tokamh <73889436+tokamh@users.noreply.github.com>
2022-05-25 11:42:59 -0700

    Update csp_in_progress.py

--- csp_in_progress.py
+++ csp_in_progress.py
@@ -1,1 +1,12 @@
+class Problem(object):
+    def __init__(self, initial, goal=None):
+        self.initial = initial
+        self.goal = goal
 
+    def result(self, state, action):
+        raise NotImplementedError
+
+    def value(self, state):
+        raise NotImplementedError
+
+   
