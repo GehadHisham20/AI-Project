a247a020c6d208cc6e2c1695f911aba9ed0ce05e
tokamh <73889436+tokamh@users.noreply.github.com>
2022-05-25 11:43:25 -0700

    Update csp_in_progress.py

--- csp_in_progress.py
+++ csp_in_progress.py
@@ -10,3 +10,62 @@ class Problem(object):
         raise NotImplementedError
 
    
+class CSP(Problem):
+    def __init__(self, variables, domains, neighbors, constraints):
+        variables = variables or list(domains.keys())
+
+        self.variables = variables
+        self.domains = domains
+        self.neighbors = neighbors
+        self.constraints = constraints
+        self.initial = ()
+        self.curr_domains = None
+        self.nassigns = 0
+    
+    def infer_assignment(self):
+        self.support_pruning()
+        return {v: self.curr_domains[v][0]
+                for v in self.variables if 1 == len(self.curr_domains[v])}
+    
+    def assign(self, var, val, assignment):
+        assignment[var] = val
+        self.nassigns += 1
+
+    def unassign(self, var, assignment):
+        if var in assignment:
+            del assignment[var]
+
+    def nconflicts(self, var, val, assignment):
+        def conflict(var2):
+            return (var2 in assignment and
+                    not self.constraints(var, val, var2, assignment[var2]))
+        return count(conflict(v) for v in self.neighbors[var])
+
+    def goal_test(self, state):
+        assignment = dict(state)
+        return (len(assignment) == len(self.variables)
+                and all(self.nconflicts(variables, assignment[variables], assignment) == 0
+                        for variables in self.variables))
+
+    def support_pruning(self):
+       if self.curr_domains is None:
+            self.curr_domains = {v: list(self.domains[v]) for v in self.variables}
+
+    def suppose(self, var, value):
+        self.support_pruning()
+        removals = [(var, a) for a in self.curr_domains[var] if a != value]
+        self.curr_domains[var] = [value]
+        return removals
+
+    def prune(self, var, value, removals):
+        self.curr_domains[var].remove(value)
+        if removals is not None:
+            removals.append((var, value))
+
+    def choices(self, var):
+        #Return all values for var that aren't currently ruled out
+        return (self.curr_domains or self.domains)[var]
+
+    def restore(self, removals):
+        for B, b in removals:
+            self.curr_domains[B].append(b)
